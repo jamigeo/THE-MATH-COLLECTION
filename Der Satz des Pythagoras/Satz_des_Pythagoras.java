f37989e4a6ab8adffed7d611206bc4317ab10452
[jamigeo] <Gjmbo@protonmail.com>
2023-05-08 10:47:31 +0200

    updated

--- Der Satz des Pythagoras/Satz_des_Pythagoras.java
+++ Der Satz des Pythagoras/Satz_des_Pythagoras.java
@@ -4,7 +4,7 @@ public class Pythago {
     public static void main(String[] args) {
         float a = 3.0F;
         float b = 4.0F;
-        float c = (float) Math.sqrt(a * a + b * b);
+        float c = (float) Math.sqrt( a * a + b * b );
         System.out.println(c);
     }
 }
