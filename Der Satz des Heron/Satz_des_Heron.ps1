d1c0f35a5d854c1bce075d696fa28ae0d3ee749d
[jamigeo] <Gjmbo@protonmail.com>
2023-05-08 10:15:10 +0200

    updated

--- Der Satz des Heron/Satz_des_Heron.ps1
+++ Der Satz des Heron/Satz_des_Heron.ps1
@@ -4,7 +4,7 @@ function Get-Area($a, $b, $c) {
     $b = 4
     $c = 5
     $s = ( $a + $b + $c ) / 2;
-    return [System.Math]::sqrt( $s * ( $s - $a ) * ( $s - $b ) * ( $s - $c ));
+    return [System.Math]::sqrt( $s * ( $s - $a ) * ( $s - $b ) * ( $s - $c ) );
 }
 Get-Area
 
