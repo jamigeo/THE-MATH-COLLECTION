60e47822c3586a548f6fff1eed177cacfefb2855
[jamigeo] <Gjmbo@protonmail.com>
2023-05-08 10:43:24 +0200

    updated

---
+++ Der Satz des Pythagoras/Satz_des_Pythagoras.java
@@ -0,0 +1,10 @@
+import java.lang.Math;
+
+public class Pythago {
+    public static void main(String[] args) {
+        float a = 3.0F;
+        float b = 4.0F;
+        float c = (float) Math.sqrt(a * a + b * b);
+        System.out.println(c);
+    }
+}
