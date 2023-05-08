cf75443f87e4a59ed4d62147ccc59a55324a600b
[jamigeo] <Gjmbo@protonmail.com>
2023-05-08 12:35:25 +0200

    updated

---
+++ Der Satz des Heron/Satz_des_Heron.java
@@ -0,0 +1,13 @@
+import java.lang.Math;
+
+public class Areal {
+    public static void main(String[] args) {
+        double a = 3.0;
+        double b = 4.0;
+        double c = 5.0;
+        double s = ( a + b + c ) / 2;
+        float real = (float) Math.sqrt( s * ( s - a ) * ( s - b ) * ( s - c ) );
+        System.out.println(real);
+    }
+}
+
