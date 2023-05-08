0a3766749bdb1e5a30501a7dfc197f847425b72c
[jamigeo] <Gjmbo@protonmail.com>
2023-05-08 10:12:26 +0200

    updated

---
+++ Der Satz des Heron/Satz_des_Heron.ps1
@@ -0,0 +1,10 @@
+
+function Get-Area($a, $b, $c) {
+    $a = 3
+    $b = 4
+    $c = 5
+    $s = ( $a + $b + $c ) / 2;
+    return [System.Math]::sqrt( $s * ( $s - $a ) * ( $s - $b ) * ( $s - $c ));
+}
+Get-Area
+
---
+++ Der Satz des Heron/Satz_des_Heron.py
@@ -0,0 +1,11 @@
+import math
+
+def get_area():
+    a = 3
+    b = 4
+    c = 5
+    s = ( a + b + c ) / 2
+    return math.sqrt( s * (s - a) * ( s - b ) * ( s - c ) )
+
+area = get_area()
+print(area)
